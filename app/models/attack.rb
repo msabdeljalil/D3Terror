b3868fb57d0f2bc7e396e7a4802a0c3bd31f9584
Apprentice <apprentice@dbc-chicago-015.local>
2014-05-08 20:15:11 -0500

    Added csv parsing method

--- app/models/attack.rb
+++ app/models/attack.rb
@@ -1,3 +1,19 @@
 class Attack < ActiveRecord::Base
-  validates :attacks, :presence => true
+  require 'CSV'
+  COUNTRY_COLUMN = 12
+  CITY_COLUMN = 15
+  DATE_COLUMN = 5
+
+  def self.import(file)
+    attack_record = []
+    CSV.foreach(file, headers: true) do |row|
+      self.create(
+        country: row[COUNTRY_COLUMN],
+        city: row[CITY_COLUMN],
+        attack_date: row[DATE_COLUMN]
+        )
+      return if self.last.id == 500
+    end
+  end
+
 end
